a44536c93c4f1e55d5a21b15a32a73b7d35628c2
receme <tahmid1989@gmail.com>
2016-03-01 23:02:52 +0600

    location fragment automatically updated

--- smartwallet/ViewController.swift
+++ smartwallet/ViewController.swift
@@ -9,17 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var _currentView:UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var mainview = MainView();
+        
+        let orientation = UIApplication.sharedApplication().statusBarOrientation
+        self.setUpViewForOrientation(orientation)
+
+    }
+    
+    override func awakeFromNib() {
+        
+        
+
+    }
+    
+    override func willRotateToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
+        
+        setUpViewForOrientation(toInterfaceOrientation)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func setUpViewForOrientation(orientation: UIInterfaceOrientation) ->Void{
+        
+        //_currentView.removeFromSuperview()
+        if(UIInterfaceOrientationIsLandscape(orientation))
+        {
+            self.view.addSubview(mainview)
+            //_currentView = _landscapeView
+        }
+        else
+        {
+            self.view.addSubview(_portraitView)
+            //_currentView = _portraitView
+        }
+    }
+    
+    
 
-
+    @IBAction func AddExpense(sender: UIButton) {
+    }
 }
 
